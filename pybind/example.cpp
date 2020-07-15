e5983df180ef2f62fcebfe2625a6add7f354ff52
bj.datemoon <hubo@localhost.localdomain>
2020-07-15 18:53:08 +0800

    add numpy input

--- pybind/example.cpp
+++ pybind/example.cpp
@@ -1,5 +1,9 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <iostream>
+#include <string.h>
 
+namespace py = pybind11;
 class Test
 {
 private:
@@ -12,6 +16,10 @@ public:
 	int Add()
 	{
 		return x+y;
+	}
+	void Print(char *str)
+	{
+		std::cout << str <<std::endl;
 	}
 };
 
@@ -42,5 +50,54 @@ PYBIND11_MODULE(example, m)
 
 	m.def("Init", &Init, "A function which init Test class");
 	m.def("Destory", &Destory, "A function which destory Test class");
-}
+	//m.def("Print", &Print, "A function which Print str");
+	//m.def("Print", [](char *str)
+	m.def("Print", [](py::object *obj) 
+		{
+		        char* str =(char*) obj->cast<std::string>().c_str();
+			//py::buffer_info info = str.request();
+			//if (info.format != py::format_descriptor<char>::format())
+			//{
+		//		std::cerr << "Expected format: "
+		//			<<  py::format_descriptor<char>::format() << "\n"
+		//			<< "Current format: " << info.format <<std::endl;
+		//	}
+		//	if (info.ndim != 1)
+		//	{
+		//		std::cerr << "Expected dim: 1\n"
+		//			<< "Current dim: " << info.ndim <<std::endl;
+		//	}
+		//	char * tmp = reinterpret_cast<char *>(info.ptr);
+			std::cout << str << std::endl;
+			memcpy(str,"test ok",8);
+			std::cout << str << std::endl;
+		});
+	m.def("SetNumpyZero",[](py::buffer b)
+		{
+			py::buffer_info info = b.request();
+			if (info.format != py::format_descriptor<float>::format()) {
+				std::cout << "Expected format: "
+					<< py::format_descriptor<float>::format() << "\n"
+					<< "Current format: " << info.format << std::endl;
+			}
+			if (info.ndim != 2) {
+				std::cout << "Expected dim: 2\n"
+					<< "Current dim: " << info.ndim <<std::endl;
+			}
+			int strides = info.strides[0] / sizeof(float);
+			float *ptr = reinterpret_cast<float*>(info.ptr);
+			for(int r=0;r<info.shape[0];r++)
+			{
+				for(int c=0;c< info.shape[1];c++)
+					std::cout << ptr[r*strides+c] << " " ;
+				std::cout <<"\n" <<std::endl;
+			}
+			for(int r=0;r<info.shape[0];r++)
+			{
+				for(int c=0;c< info.shape[1];c++)
+					ptr[r*strides+c] = 0;
+			}
+		});
 
+
+}
--- pybind/run.py
+++ pybind/run.py
@@ -1,8 +1,25 @@
 import example
-
+import numpy as np
 
 
 handle=example.Init(1,2)
 
 print(example.Add(handle))
+test_str='test print'
+example.Print(test_str)
+print(test_str)
 example.Destory(handle)
+
+nptest=np.array(range(10),dtype=np.float32).reshape(2,5)
+print(nptest)
+example.SetNumpyZero(nptest)
+print(nptest)
+
+def Print(str1):
+    print(str1)
+    str1='test ok'
+    print(str1)
+
+print('-----------')
+Print(test_str)
+print(test_str)
